d1dbfb2095162bb904989dccd6c4fe6d8876cc56
NIGHTCORE0101 <azzamswafi2020@gmail.com>
2025-02-25 15:22:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Message sent! Thank you for contacting us.");
+  });
+
+alert("selmat datang di web artikel Azzam Shalahuddin Wafi");
